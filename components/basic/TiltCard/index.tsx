d38d4b9ca37e88a3115cf82dd329083b28a829b8
Jun Hong <klee.junhong@gmail.com>
2024-01-08 00:19:42 +0800

    minor: fixed size issue

--- components/basic/TiltCard/index.tsx
+++ components/basic/TiltCard/index.tsx
@@ -15,7 +15,7 @@ const TiltCard = ({
   className?: string;
 }) => {
   return (
-    <Tilt className={`2xl:w-[240px] xl:w-[220px] sm:w-[200px] xs:w-[150px] w-[100px] ${className}`}>
+    <Tilt className={`xl:w-[210px] sm:w-[200px] xs:w-[150px] w-[100px] ${className}`}>
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.7)}
         className="w-full card-border-gradient p-1 rounded-xl shadow-xm"
