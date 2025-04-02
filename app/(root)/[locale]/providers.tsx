0299babb1faeacbeab67e06bc4b1f7693f22de5e
Jun Hong <klee.junhong@gmail.com>
2025-04-02 11:09:05 +0800

    feat: update vulnerable packages, changed default theme to be based on system

--- app/(root)/[locale]/providers.tsx
+++ app/(root)/[locale]/providers.tsx
@@ -2,12 +2,12 @@
 
 import { ThemeProvider } from "next-themes";
 import StyledComponentsRegistry from "@lib/AntdRegistry";
-import {AntdTheme} from "@styles/antdStylesConfig";
+import { AntdTheme } from "@styles/antdStylesConfig";
 import { ConfigProvider } from "antd";
 
 export function StyleProviders({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light">
+    <ThemeProvider attribute="class" enableSystem={true} defaultTheme="system">
       <StyledComponentsRegistry>
         <ConfigProvider theme={AntdTheme}>{children}</ConfigProvider>
       </StyledComponentsRegistry>
